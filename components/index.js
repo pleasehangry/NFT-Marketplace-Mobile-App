0d915d197e9b1190a849ab9fcd765e9f58d59cf7
= <=>
2022-12-25 14:22:58 +0700

    Details Screen

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatus from "./FocusedStatus";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfor } from "./SubInfor";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+  HomeHeader,
+  FocusedStatus,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfor,
+  DetailsBid,
+  DetailsDesc,
+};
